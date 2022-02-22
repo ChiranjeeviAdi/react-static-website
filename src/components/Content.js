f2d1684a8d12c8e865940f48dc37fbce25d34b73
Chiranjeevi <chiranjeeviadi@gmail.com>
2022-02-22 09:14:02 +0530

    Parent Child Static website using react

---
+++ src/components/Content.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function ContentComponent(){
+
+    return (
+        <div className="contentDiv">
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li><h3>Was first introduced 2013</h3></li>
+                <li><h3>Was Orginially developer in google by Jordon</h3></li>
+                <li><h3>100 Stars in github</h3></li>
+                <li><h3>Is maintained by facebook</h3></li>
+                <li><h3>Thousands of enterprize applications build</h3></li>
+            </ul>
+        </div>
+    )
+}
